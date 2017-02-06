4adf32dad8a61e23364fd049ffe1a7af9e7b7d9b
BrendanReterink <brendan.reterink@student.hu.nl>
2017-02-06 11:21:11 +0100

    Eindopdracht

--- Eindopdracht/Website kopie/registreren.js
+++ Eindopdracht/Website kopie/registreren.js
@@ -2,21 +2,7 @@
 
 $(document).ready(function(){
    
-function previewFile(){
-       var preview = document.querySelector('img'); //selects the query named img
-       var file    = document.querySelector('input[type=file]').files[0]; //sames as here
-       var reader  = new FileReader();
 
-       reader.onloadend = function () {
-           preview.src = reader.result;
-       }
-
-       if (file) {
-           reader.readAsDataURL(file); //reads the data as a URL
-       } else {
-           preview.src = "";
-       }
-  }
 
 $("#knopaanmelden").click(function(){ 
 var voornaam = $('#voornaam').val();
